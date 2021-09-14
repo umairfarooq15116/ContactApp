f08129d636564ae8a1f9ea0ea19947deab488e7a
umairfarooq15116 <umair.pucit16@gmail.com>
2021-09-14 22:32:15 +0500

    1) Generic Repository Structure is designed. 2) ConnectionString also defined.

---
+++ Contact/Repository/Generic/GenericRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact.Repository.Generic
+{
+    class GenericRepository<T> : IGenericRepository<T> where T: class
+    {
+        public void Delete(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetByID(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
